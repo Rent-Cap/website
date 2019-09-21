9d23c32a92066af2958812ae9dee82b838f2a3b8
Joshua Nelson <joshuanelson@Hypnos.local>
2019-09-21 14:58:17 -0700

    Add an address component

---
+++ components/address.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Autocomplete from "react-google-autocomplete";
+
+const API_KEY = "AIzaSyA2zJGf2oJthhaFoUXjT3365O1u7qdZQNg";
+
+const Address = () => (
+  <>
+    <script
+      type="text/javascript"
+      src={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`}
+    ></script>
+
+    <Autocomplete
+      style={{ width: "90%" }}
+      onPlaceSelected={place => {
+        console.log(place);
+      }}
+      types={["address"]}
+      componentRestrictions={{ country: "us" }}
+    />
+  </>
+);
+
+export default Address;
