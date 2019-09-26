3220bbe53ace31ebe2eeabf2a00d3a149303f6cd
Josh Nelson <nelsonjoshpaul@gmail.com>
2019-09-25 22:19:43 -0700

    Fix SSR of the Address component

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,33 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Autocomplete from "react-google-autocomplete";
+import Helmet from "react-helmet";
 
 const API_KEY = "AIzaSyA2zJGf2oJthhaFoUXjT3365O1u7qdZQNg";
 
 const Address = () => {
-  const [google, setGoogle] = useState();
+  const [shouldRender, setShouldRender] = useState();
+  const [isGoogleReady, setIsGoogleReady] = useState();
+
   useEffect(() => {
-    if (google) return;
-    if (typeof window !== `undefined` && window.google) {
-      setGoogle(google);
+    setShouldRender(true);
+  }, [shouldRender]);
+
+  useEffect(() => {
+    window.initGoogleMapsApi = () => {
+      setIsGoogleReady(true);
     }
-  });
+  }, [isGoogleReady]);
   return (
     <>
-      <script
-        type="text/javascript"
-        src={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`}
-      ></script>
-
-      {google ? (
-        <Autocomplete
-          style={{ width: "100%" }}
-          onPlaceSelected={place => {
-            console.log(place);
-          }}
-          types={["address"]}
-          componentRestrictions={{ country: "us" }}
-        />
-      ) : null}
+    {shouldRender ? (
+      <>
+        <Helmet>
+          <script
+            type="text/javascript"
+            src={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places&callback=initGoogleMapsApi`}
+            async defer
+          ></script>
+        </Helmet>
+        {isGoogleReady ? (
+          <Autocomplete
+            style={{ width: "100%" }}
+            onPlaceSelected={place => {
+              console.log(place);
+            }}
+            types={["address"]}
+            componentRestrictions={{ country: "us" }}
+          />
+        ) : null}
+      </>
+    ) : null }
     </>
   );
 };
