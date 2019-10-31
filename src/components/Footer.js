a17003915d58bfe3df690f3756875057d3e87b9f
Reid Sherman <reidsherman@gmail.com>
2019-10-30 21:11:00 -0700

    Style age check page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 
-const StyledFooter = styled.footer`
-    text-align: center;
-    font-size: 18px;
-    grid-column: 4 / span 8;
-    font-weight: 200;
-    color: #979797;
-`
-
-const Footer = () => 
-<StyledFooter>
+const Footer = () =>
+<footer>
     The information contained on this website is provided for informational purposes only, and should not be considered legal advice on any matter
-</StyledFooter>;
-
+</footer>
 export default Footer;
