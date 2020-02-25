dde1a07d7e8fa42dd2ed873b6c98347e9ec238f2
Tom Croucher <tom.croucher@gmail.com>
2020-02-25 07:49:27 -0800

    update calc tests

--- src/methods/rentCalculator.js
+++ src/methods/rentCalculator.js
@@ -14,9 +14,18 @@
  *
  */
 
-// returns array of tuples of monthly rents based on history
-// [{month: "2019-03", rent: 140000, delta: 400}, ... ]
-// month is ISO Date month ("yyyy-MM"), rent is int of price in cents, delta is change in rent from previous month
+/**
+ * rentHistooryToMonthlyRents - returns rent for each month from various inputs
+ * 
+ * @param {number} rentOn20190315 - The rent on 2019-03-15
+ * @param {number} initialRent - The initial rent paid by the tenant in cents
+ * @param {string} tenancyStartDate - The start date of the tenancy as a datestring (date objects won't break anything)
+ * @param {array} rentChanges - An array of objects specifying changes to the rent as {"month":"2019-12", "rent":200000}
+ * @param {string} currentDate - The current date a datestring e.g. "2020-02-12" (date objects won't break anything)
+ * 
+ * @return {object} The rent each moonth indexed by date string {"2019-03":150000, ..., "2019-12": 200000} up to the current month
+ *
+ */
 export function rentHistoryToMonthlyRents(
   rentOn20190315,
   initialRent,
@@ -74,40 +83,85 @@ export function rentHistoryToMonthlyRents(
 // returns array of maximum allowable rent each month starting max(2019-03, tenancyStartDate, current date - 1y)
 // [{month: "2019-03", maxRent: 140000}, ... ]
 // month is ISO Date month ("yyyy-MM"), rent is int of price in cents
+/**
+ * calculateMaxRents - returns the max rent under AB1482 for each month based on the location of the zip code
+ * 
+ * @param {number} zip - A 5 digit CA zip code
+ * @param {object} monthlyRents - An object of rents indexed by month string returned from rentHistoryToMonthlyRents() e.g. {"2019-03": 150000, ...}
+ * 
+ * @return {object} The max AB1482 rent each month indexed by date string {"2019-03":Infinity, ..., "2020-01": 165000} up to the current month. Infinity indicates no limit on rent
+ *
+ */
 export function calculateMaxRents(zip, monthlyRents) {
   let maxRents = {};
   let rentMonths = Object.keys(monthlyRents).sort();
   let index201903;
 
   for (let i = 0; i < rentMonths.length; i++) {
+    let maxRent = {
+      maxRent: null,
+      rollbackExempt: false
+    };
     let month = rentMonths[i];
 
-    if (month < "2020-01") {
-      // until 2020-01 there was no cap
-      if (month === "2019-03") {
-        index201903 = i; // track this index for later
-      }
-      // TODO (@sh1mmer) fix assumption here about rent being the same for all of 2019-03 rather than before/after 2019-03-15
-      maxRents[month] = -1; // -1 for infinity
-      continue;
+    if (i == 0) {
+      // if this is the first rent month then this is the epoch rent so it is the max
+      maxRent.maxRent = monthlyRents[rentMonths[i]];
     }
 
-    // look back 12 months for lowest rent or from 2019-03 if a year hasn't passed
-    // TODO (@sh1mmer) tidy this up later when it isn't needed
-    startLookback = month < "2020-04" ? index201903 : i - 12;
+    if (month === "2019-03") { 
+      index201903 = i; // keep index position to use for setting the lookback window later
+      
+      // if the month is 2019-03 set the epoch rent max to this rent annd not add an increase
+      // TODO (@sh1mmer) this doesn't account for before after 2019-03-15 rent dates
+      // e.g. if rent was due on 2019-03-20 then 2019-02 rent might be the legal max not rent paid in 2019-03
+      maxRent.maxRent = monthlyRents[rentMonths[i]]; 
+    }
 
-    let lowestRent = monthlyRents[startLookback];
-    let lowestRentMonth = rentMonths[startLookback];
-    for (let j = startLookback + 1; j < i; j++) {
+    if (month < "2019-03") {
+      // until 2019-03 there was no cap so those months have an infinite cap and are exempt from rollback
+      maxRent.maxRent = Infinity;
+    }
+    
+    if (month < "2020-01") {
+      // before 2020-01 months are exmpet from rollbacks
+      maxRent.rollbackExempt = true;
+    }
+
+    /* for each rent period we need to calculate how far to look back for lowest rent
+     * N.B. the 2019-03 code is still applicable after 2020-04 since we need to be able to still calculate past
+     * illegal increases starting 2019-03 to calculate the correct current rollback rent
+    */
+    let startLookback = (month < "2020-04" && index201903) ? index201903 : i - 12; // use 2019-03 rent if we have it rent have it before 2020-04
+    startLookback = (startLookback < 0) ? 0 : startLookback; // if lookback is further than rent history start at first rent month 
+    
+    // find the lowest rent and its month in the lookback range
+    var lowestRentMonth = rentMonths[startLookback];
+    var lowestRent = monthlyRents[lowestRentMonth];
+    for (let j = startLookback + 1; j <= i; j++) {
+      // make sure we include i so if the rent lowers we can reset to the current month
       if (monthlyRents[rentMonths[j]] < lowestRent) {
         lowestRentMonth = rentMonths[j];
-        lowestRent = monthlyRents[rentMonths[j]];
+        lowestRent = monthlyRents[lowestRentMonth];
       }
     }
 
-    let cpi = cpiChangeLookup(zip, lowestRentMonth); // lookup CPI for the month of lowest rent in this zip
-    let maxModifier = Math.min(cpi + 0.05, 0.1); // use least of cpi + 5% or 10%
-    let maxRent = monthlyRents[lowestRentMonth] * (1 + maxModifier);
+    // if we didn't already set a maxRent for this month we should set one
+    if (maxRent.maxRent === null) {
+      if (month === lowestRentMonth) {
+        // if this is the lowest rent month don't add the multiplier
+        // this is important for the case when rent decreases
+        maxRent.maxRent = lowestRent;
+      } else {
+        // TODO (@sh1mmer) we might want a way to inject this CPI calc function especially if this gets a remote data driver later
+        let cpi = cpiChangeLookup(zip, lowestRentMonth); // lookup CPI for the month of lowest rent in this zip
+        let maxModifier = Math.min(cpi + 0.05, 0.1); // use least of cpi + 5% or 10%
+        let maxRentAmt = monthlyRents[lowestRentMonth] * (1 + maxModifier);
+        maxRentAmt = Number(maxRentAmt.toFixed(0)); // TODO (@sh1mmer) decide about rounding and precision here
+        maxRent.maxRent = maxRentAmt;
+      }
+    }
+
     maxRents[month] = maxRent;
   }
 
@@ -122,8 +176,8 @@ export function findFirstIllegalIncreaseMonth(monthlyRents, maxRents) {
   // TODO (@sh1mmer) add something to find illegal increase based on number of increases in 12mo period
   for (let i = 0; i < rentMonths.length; i++) {
     let month = rentMonths[i];
-    if (monthlyRents[month] > maxRents[month] && maxRents[month] !== -1) {
-      // exclude months before 2020-01-01 since they hve no legal max (e.g. -1)
+    if (monthlyRents[month] > maxRents[month].maxRent && !maxRents[month].rollbackExempt) {
+      // exclude months before 2020-01-01 since they hve no legal max (e.g. Infinity)
       return month;
     }
   }
@@ -131,7 +185,7 @@ export function findFirstIllegalIncreaseMonth(monthlyRents, maxRents) {
   return null;
 }
 
-export function calculateRefunds(monthlyRents, maxRents, currentDate) {
+export function calculateOverpayments(monthlyRents, maxRents, currentDate) {
   // returns array of months entitled to a refund
   // [{month: "2019-03", maxRent: 140000}, ... ]
   // month is ISO Date month ("yyyy-MM"), rent is int of price in cents
@@ -140,13 +194,7 @@ export function calculateRefunds(monthlyRents, maxRents, currentDate) {
     maxRents
   );
 
-  if (firstIllegalIncreaseMonth === null) {
-    // we don't have any months in which the rent was too damn high (legally speaking)
-    // TODO need to make the array still. jeez.
-    return 0;
-  }
-
-  // TODO get first illegal rent and then calc the delta from rent paid to the previous legal amount
+  // TODO (@sh1mmer) get first illegal rent and then calc the delta from rent paid to the previous legal amount
   let rentMonths = Object.keys(monthlyRents).sort();
   let lastLegalRent;
   let overPayments = {};
@@ -154,20 +202,19 @@ export function calculateRefunds(monthlyRents, maxRents, currentDate) {
   for (let i = 0; i < rentMonths.length; i++) {
     let month = rentMonths[i];
     // step until we get to the first illegal increase month n.b. this can never be month 0
-    if (month < firstIllegalIncreaseMonth) {
+    if (firstIllegalIncreaseMonth === null || month < firstIllegalIncreaseMonth) {
       overPayments[month] = 0;
       continue;
     }
-    if (month >= firstIllegalIncreaseMonth) {
+    if (month == firstIllegalIncreaseMonth) {
       // if this is the first month with illegal rent then we step backwards from here through the rents paid
-      // to find the first rent less than the maximum allowed for the first illegal increase to use as the rollback rent
+      // to find last time rent was less than the maximum allowed for the first illegal increase to use as the rollback rent
       // the rollback amount is _not_ the maxRent allowed but the last rent below the max
       for (let j = i - 1; j >= 0; j--) {
         let rollbackMonth = rentMonths[j];
-        if (monthlyRents[rollbackMonth] < maxRents[firstIllegalIncreaseMonth]) {
-          // TODO (@sh1mmer) after Jun 15 2020 this should always be i-1 (or it wouldn't be the first illegal increase)
-          // before Jun 15 2020 since increase above the cap for 2020-01-01 were legal from 2019-03-15 to 2019-12-31
-          // we want to be able to step back past i-1 if needed to find a rent paid less than the cap
+        if (monthlyRents[rollbackMonth] < maxRents[rollbackMonth].maxRent || // if this month was under the limit
+            rollbackMonth === "2019-03" || // or if we hit 2019-03
+            j == 0) { // or we hit the first month of the rental
           // TODO (@sh1mmer) validate how this works with multiple increases in 12mo period
           lastLegalRent = monthlyRents[rollbackMonth];
           break;
@@ -182,16 +229,17 @@ export function calculateRefunds(monthlyRents, maxRents, currentDate) {
   return overPayments;
 }
 
-export function calculateRollback(monthlyRents, maxRents, currentDate) {
-  // returns (int) cents of rent the user should be paying on current date // if the current rent is legal then it returns 0
-  let overPayments = calculateRefunds(monthlyRents, maxRents, currentDate);
+export function calculateRollbackRent(monthlyRents, maxRents, currentDate) {
+  // returns (int) cents of rent the user should be paying on current date
+  // if the current rent is legal then it returns the current rent
+  let overPayments = calculateOverpayments(monthlyRents, maxRents, currentDate);
   let currMonth = dateToISOMonth(currentDate);
 
   let rollback = monthlyRents[currMonth] - overPayments[currMonth];
-  return rollback === monthlyRents[currMonth] ? 0 : rollback;
+  return (rollback === monthlyRents[currMonth]) ? monthlyRents[currMonth] : rollback;
 }
 
-function calculateRefund(overPayments, currentDate) {
+export function calculateRefund(overPayments, currentDate) {
   // returns (int) cents of the total refund amount based on any monthly rents that were over the maxRents
   // we can just sum the over payments array
   return Object.values(overPayments).reduce(function(a, b) {
