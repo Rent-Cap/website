b292deecfa4c2d34db5619e6a4c4e375fc7bbbad
Wayne Li <wayneli49@gmail.com>
2021-08-02 14:29:27 -0700

    Remove gatsby-plugin-manifest as a dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,18 +27,6 @@ module.exports = {
         path: `${__dirname}/src/pages`
       }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "Rent Cap",
-        short_name: "Rent Cap",
-        start_url: "/",
-        // background_color: "#663399",
-        // theme_color: "#663399",
-        display: "minimal-ui",
-        icon: "src/images/rentcap5.svg" // This path is relative to the root of the site.
-      }
-    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`
   ]
